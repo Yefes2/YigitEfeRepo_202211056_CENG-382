1e142992628a711166330fada07eae91f7af8c2a
Yefes2 <yefesalgin@gmail.com>
2026-03-27 00:49:55 +0300

    Add MyRazorAuthApp initial Razor Pages project

---
+++ 202211056_Web-Project/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _202211056_Web_Project.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
